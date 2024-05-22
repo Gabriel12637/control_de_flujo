b0bcf519aa8d9e8e1fcd6d69b8ba4152b0a0e7ff
hackanonimous <gabriel12637>
2024-05-22 10:15:36 -0500

    archivo

---
+++ proyecto/README.md
@@ -0,0 +1,8 @@
+# sistema de control de grass sintetico
+el usuario tiene un grass el cual administra de manera manual , el usuario lo necesita que se automatize el proceso de la reserva y pago del alquiler , para cual se solicita los siguientyes casos de uso
+
+1 el usuario podra ver las lista de horario disponible
+2 el usuario podra reservar en uno de los horarios disponibles 
+3 el usuario podra pagar por el alquiler de la reserva realizada
+4 el usuario podra verificar su alquiler el cual mostrar los detalles 
+  como la fecha, hora y costodel alquiler que realizo
---
+++ proyecto/main.py
@@ -0,0 +1,19 @@
+"horarios_disponible" = ["9:00AM","11:00AM","3:00PM","5:00PM"]
+"defmostar_horario"():
+print("horarios_disponibles:")
+for horario in ("horarios_disponibles"):
+    print(horario)
+
+def reservar_horario(horario_elegido):
+    if horario_elegido in ("horarios_disponible")
+    "retum" ("horario_elegido")
+else:
+"retum" (horario no valido)
+
+def pagar_alquiler(costo_alquiler):
+    print(f"!pago por alquiler por $[costo_alquiler] realisado con exito!")
+    "mostrar" (horario) ()
+    horario_seleccionado=reservar_horario("11:00AM")
+    print(f"horario reservado:[horario_seleccionado]")
+    costo_alquiler=50
+    pagar_alquiler(costo_alquiler)
